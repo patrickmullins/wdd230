75dccb16d8bd8b75dca431a5bb98c28f1a45d70c
patrickmullins <pmullins@byui.edu>
2022-04-25 12:31:46 -0400

    Fixed last modified and current year script.

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const date2 = document.querySelector('#currentdate2');
-try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric')};
-    date2.textContent = new Date().toLocaleDateString('en-US', options);
-}
-catch (e) {
-    alert('Error with code or your browser does not support Locale');
-}
