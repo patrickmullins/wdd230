1c88098f4617a988c1f8ffb1dfdbbf80e1b61e36
patrickmullins <pmullins@byui.edu>
2022-04-25 07:27:37 -0400

    fixed document.write issue

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 var date = document.lastModified;
-document.write("Last Modified: " + date);
+parentNode.insertBefore("Last Modified: " + date);
