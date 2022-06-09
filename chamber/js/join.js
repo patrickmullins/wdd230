a2f59a62b225f94f465e11146d2f7f00a203a3e4
patrickmullins <pmullins@byui.edu>
2022-06-09 13:11:51 -0400

    join page second commit

---
+++ chamber/js/join.js
@@ -0,0 +1,16 @@
+// Open thankyou.html when user submits
+let sayThankYou = () => {
+    window.open('https://patrickmullins.github.io/wdd230/chamber/thankyou.html',
+    '_blank');
+};
+
+document.querySelector('.join-submit-btn').addEventListener('click', 
+    sayThankYou);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760)
+    {
+        mainNav.classList.remove()
+    }
+};
