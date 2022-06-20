fb49dccf996c2d7d6d9e8cd861ade7d7c262279a
patrickmullins <pmullins@byui.edu>
2022-06-20 02:37:44 -0400

    Weather API test is functional

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -4,11 +4,16 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 
-const url = "...";
+const url = "http://api.openweathermap.org/data/2.5/weather?id=5861897&q=Fairbanks&units=imperial&appid=860f9926ff8529c24eb7b87aae40eeec";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
     console.log(data); // this is temporary for development only
-    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
-    
+    document.querySelector("#current-temp").textContent = data.main.temp;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+		document.querySelector("#weathericon").setAttribute("src", iconsrc);
+		document.querySelector("#weathericon").setAttribute("alt", desc);
+		document.querySelector("figcaption").textContent = desc;
+   
   });
