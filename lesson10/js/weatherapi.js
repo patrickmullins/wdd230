48ac4aa4c5b0f9b1d2d5fa73b0f634fb39078a86
patrickmullins <pmullins@byui.edu>
2022-06-20 02:54:07 -0400

    change url settings

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,7 +4,7 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 
-const url = "http://api.openweathermap.org/data/2.5/weather?id=5861897&q=Fairbanks&units=imperial&appid=860f9926ff8529c24eb7b87aae40eeec";
+const url = "https://api.openweathermap.org/data/2.5/weather?id=5861897&q=Fairbanks&units=imperial&appid=860f9926ff8529c24eb7b87aae40eeec";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
@@ -12,7 +12,7 @@ fetch(url)
     document.querySelector("#current-temp").textContent = data.main.temp;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     const desc = data.weather[0].description;
-    
+
     document.querySelector("#icon-src").textContent = iconsrc;
 		document.querySelector("#weathericon").setAttribute("src", iconsrc);
 		document.querySelector("#weathericon").setAttribute("alt", desc);
