2c9d427eec5eab0c9d2bb9f697b21797e66dc1a1
patrickmullins <pmullins@byui.edu>
2022-07-06 19:30:30 -0400

    Navigation bar working

--- finalProject/js/script.js
+++ finalProject/js/script.js
@@ -1,4 +1,7 @@
 const year = document.querySelector(".one").textContent = new Date().getFullYear();
+// Hamburger menu
+const hambutton = document.querySelector('.hamburger');
+const mainNav = document.querySelector('.link-list')
 
 hambutton.addEventListener('click', () => 
     {mainNav.classList.toggle('responsive')}, false);
