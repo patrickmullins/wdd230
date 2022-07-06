7413d0d09e95d72f7b7b33c09cae038ad17c1f64
patrickmullins <pmullins@byui.edu>
2022-07-06 19:21:06 -0400

    named pages for final project

--- finalProject/js/script.js
+++ finalProject/js/script.js
@@ -1,1 +1,6 @@
 const year = document.querySelector(".one").textContent = new Date().getFullYear();
+
+hambutton.addEventListener('click', () => 
+    {mainNav.classList.toggle('responsive')}, false);
+ 
+
